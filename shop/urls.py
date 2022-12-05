11ca441c0e9705e1b7710423554280b316d8610a
Sultan Usmanov <susmanov@hostkey.com>
2022-12-06 00:09:03 +0600

    second hw

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.greetings, name='greetings'),
+    path('shop/', views.list_item, name='list_item'),
+    path('<int:id>/shop/', views.detail_item, name='detail_item'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Item, Purchase
 
 def greetings(request):
     return HttpResponse("Добро пожаловать в наш магазин!")
+
+def list_item(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request, 'list_item.html', context=context)
+
+def detail_item(request, id):
+    item = get_object_or_404(Item, id=id)
+    context = {
+        'item': item
+    }
+    return render(request, 'detail_item.html', context)
